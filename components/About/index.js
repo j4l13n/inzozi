6abedb5d77a65b07a9cb91145f298836cd02cd89
j4l13n <juliushirwa@gmail.com>
2021-08-21 11:44:13 +0200

    finalizing

--- components/About/index.js
+++ components/About/index.js
@@ -10,11 +10,11 @@ const AboutUs = () => {
       <div className="row" style={{
         paddingTop: "20px"
       }}>
-        <div className="col d-flex justify-content-center py-5">
-          <img src="/inzozi-logo.png" alt="" className="rounded shadow-sm"  />
+        <div className="col-md-6 d-flex justify-content-center py-5">
+          <img src="/inzozi-logo.png" height="250" alt="" className="rounded shadow-sm"  />
         </div>
-        <div className="col">
-          <div className="row">
+        <div className="col-md-6">
+          <div className="row px-2">
             {/* <div className="h4 py-2">About Us</div> */}
             <p className="lead">
             <b>INZOZI Tours</b> offers an adventure of a lifetime, 
@@ -26,7 +26,7 @@ const AboutUs = () => {
             experience of enjoying the vast wildlife that Rwanda has to offer.<br /><br />
             </p>
           </div>
-          <div className="row">
+          <div className="row py-2 px-2">
             <a type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" className="" style={{cursor: 'pointer'}}>Read more</a>
           </div>
         </div>
@@ -40,7 +40,7 @@ const AboutUs = () => {
             <div className="modal-body">
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 <div className="border-bottom">
-                  <img src="/inzozi-logo.png" className="" alt="" />
+                  <img src="/inzozi-logo.png" className="w-100" height="240" alt="" />
                 </div>
                 <div className="row py-2 border-bottom">
                   {/* <div className="h5 py-2">About Us</div> */}
