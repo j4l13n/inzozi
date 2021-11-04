3948933ea43e1d0c01379740a12cc03fb7b3514e
j4l13n <juliushirwa@gmail.com>
2021-11-04 18:48:38 +0200

    fix meet the founder

--- components/Founder/index.js
+++ components/Founder/index.js
@@ -3,25 +3,25 @@ const MeetFounder = () => {
     <div className="wrapper">
       <div className="about">
         <div className="about-details">
-          <div className="margin-auto">
+          <div className="margin-auto py-5">
             <h1>MEET THE FOUNDER</h1>
             <p>
-            Gasigwa Theogene was born and raised in Rwanda and he’s passionate about his homeland.
-            It’s a country that he knows and loves well, but life hasn’t been easy for him.
+              Gasigwa Theogene was born and raised in Rwanda and he’s passionate about his homeland.
+              It’s a country that he knows and loves well, but life hasn’t been easy for him.
             </p>
             <p>
-            At only two weeks old he was taken to the Noel Orphanage in Rubavu after both his parents died in the atrocities leading up to the Genocide in 1994.
-For the next twenty four years he lived a life of hardship and struggle with his ‘brothers and sisters’, in the Orphanage. He faced many challenges but he was always determined to succeed.
+              At only two weeks old he was taken to the Noel Orphanage in Rubavu after both his parents died in the atrocities leading up to the Genocide in 1994.
+              For the next twenty four years he lived a life of hardship and struggle with his ‘brothers and sisters’, in the Orphanage. He faced many challenges but he was always determined to succeed.
             </p>
             <p>
-            He excelled at school where he studied Tourism and Hospitality. At university he was proud to graduate with a Bachelor’s Degree in Hotel and Restaurant Management with Distinction.
-Opportunities followed and he gained invaluable working experience at Nyungwe Forest National Park, Lake Kivu Serena Hotel and Akagera National Park.
+              He excelled at school where he studied Tourism and Hospitality. At university he was proud to graduate with a Bachelor’s Degree in Hotel and Restaurant Management with Distinction.
+              Opportunities followed and he gained invaluable working experience at Nyungwe Forest National Park, Lake Kivu Serena Hotel and Akagera National Park.
             </p>
             <p>
-            Gasigwa’s zest for life and the love he feels for his homeland have only made him more determined to introduce others to the joys and beauty of Rwanda- The Land of a Thousand Hills.
+              Gasigwa’s zest for life and the love he feels for his homeland have only made him more determined to introduce others to the joys and beauty of Rwanda- The Land of a Thousand Hills.
             </p>
             <p>
-            From a difficult start, he knows that his success will give hope and promise to those ‘brothers and sisters’, who were there struggling with him in the beginning.
+              From a difficult start, he knows that his success will give hope and promise to those ‘brothers and sisters’, who were there struggling with him in the beginning.
             </p>
             <p>
               Now, he’s ready to show you his Rwanda.
@@ -29,7 +29,7 @@ Opportunities followed and he gained invaluable working experience at Nyungwe Fo
           </div>
         </div>
         <div>
-          <img src="/gasigwa.jpeg" alt="Gasigwa Theogene" className="w-100 p" />
+          <img src="/gasigwa.jpeg" alt="Gasigwa Theogene" className="w-100" />
         </div>
       </div>
     </div>
