7a5e20a5b5752329614920a892af189a7e2238a3
j4l13n <juliushriwa@gmail.com>
2023-01-27 20:10:24 +0200

    inzozi tours in description

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="description" content="inzozitours.rw, inzozi tours, inzozi tours rwanda, visit Rwanda" />
+          <meta name="description" content="inzozitours, inzozi tours, inzozi tours rwanda, inzozi tours rw, visit Rwanda" />
           <title>INZOZI | Adventure of a lifetime </title>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
       </Head>
